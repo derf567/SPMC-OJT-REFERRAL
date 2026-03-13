1bb89dd28584de1c406ec50d5f0eaeca0941382f
Fred <wilfredomarinay@gmail.com>
2026-03-13 09:50:27 +0800

    Jayci Changes

--- SPMC/front-end/src/pages/ActiveReferrals.tsx
+++ SPMC/front-end/src/pages/ActiveReferrals.tsx
@@ -6,8 +6,8 @@ const ActiveReferrals = () => {
     <DashboardLayout>
       <div className="space-y-6">
         <div>
-          <h1 className="text-2xl font-bold text-gray-900 dark:text-white">Active Referrals</h1>
-          <p className="text-gray-500 dark:text-gray-400">View and manage active patient referrals</p>
+          <h1 className="text-2xl font-bold text-gray-900 dark:text-white">Referral Requests</h1>
+          <p className="text-gray-500 dark:text-gray-400">View and manage incoming referral requests</p>
         </div>
         
         <ReferralTable />
